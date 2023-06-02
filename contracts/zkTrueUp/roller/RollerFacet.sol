da3f53a7ad8a19b6bf3c091c60ea7b3eada590c1
Ray Huang <ray880301@gmail.com>
2023-06-02 10:18:59 +0800

    fix console error

--- contracts/zkTrueUp/roller/RollerFacet.sol
+++ contracts/zkTrueUp/roller/RollerFacet.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.17;
 
 import {ISolidStateERC20} from "@solidstate/contracts/token/ERC20/ISolidStateERC20.sol";
 import {SafeERC20} from "@solidstate/contracts/utils/SafeERC20.sol";
+import {IWETH} from "../interfaces/IWETH.sol";
 import {LoanLib} from "../loan/LoanLib.sol";
 import {AccountLib} from "../account/AccountLib.sol";
 import {AddressLib} from "../address/AddressLib.sol";
@@ -11,6 +12,8 @@ import {LoanStorage, Loan} from "../loan/LoanStorage.sol";
 import {AssetConfig} from "../token/TokenStorage.sol";
 import {LiquidationFactor} from "../loan/LoanStorage.sol";
 import {Config} from "../libraries/Config.sol";
+
+import {console} from "hardhat/console.sol";
 
 contract RollerFacet {
     using SafeERC20 for ISolidStateERC20;
@@ -34,18 +37,26 @@ contract RollerFacet {
         LoanLib.requireHealthy(healthFactor);
 
         address aaveV3PoolAddr = AddressLib.getAaveV3PoolAddr();
-        // using ISolidStateERC20 instead of customized transferFrom because of the AAVE receive WETH instead of ETH
-        if (collateralAsset.tokenAddr != Config.ETH_ADDRESS)
-            ISolidStateERC20(collateralAsset.tokenAddr).approve(aaveV3PoolAddr, collateralAmt);
         IPool aaveV3Pool = IPool(aaveV3PoolAddr);
-        try aaveV3Pool.supply(collateralAsset.tokenAddr, collateralAmt, msg.sender, 0) {
+        address supplyTokenAddr;
+        // using ISolidStateERC20 instead of customized transferFrom because of the AAVE receive WETH instead of ETH
+        if (collateralAsset.tokenAddr == Config.ETH_ADDRESS) {
+            address wethAddr = AddressLib.getWETHAddr();
+            IWETH(wethAddr).approve(aaveV3PoolAddr, collateralAmt);
+            supplyTokenAddr = wethAddr;
+        } else {
+            ISolidStateERC20(collateralAsset.tokenAddr).approve(aaveV3PoolAddr, collateralAmt);
+            supplyTokenAddr = collateralAsset.tokenAddr;
+        }
+
+        try aaveV3Pool.supply(supplyTokenAddr, collateralAmt, address(this), 0) {
             try aaveV3Pool.borrow(debtAsset.tokenAddr, debtAmt, 2, 0, address(this)) {
                 LoanStorage.layout().loans[loanId] = loan;
             } catch {
                 revert("RollerFacet: borrow from Aave failed");
             }
         } catch {
-            revert("RollerFacet: supply collateral to Aave failed");
+            revert("AaveV3Pool: supply failed");
         }
     }
 }
