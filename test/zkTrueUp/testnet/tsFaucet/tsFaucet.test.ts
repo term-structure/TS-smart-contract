9090d58c85f1676d3ead84f3fe5e2fa79169a3fc
Ray Huang <ray880301@gmail.com>
2023-06-07 12:33:07 +0800

    move test file

--- test/zkTrueUp/testnet/tsFaucet/tsFaucet.test.ts
+++ test/zkTrueUp/testnet/tsFaucet/tsFaucet.test.ts
@@ -3,10 +3,10 @@ import { expect } from "chai";
 import { Signer, utils } from "ethers";
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { MIN_DEPOSIT_AMOUNT, TS_BASE_TOKEN } from "term-structure-sdk";
-import { FACET_NAMES } from "../../../utils/config";
-import { deployAndInit } from "../../utils/deployAndInit";
-import { useFacet } from "../../../utils/useFacet";
-import { getRandomUint256 } from "../../utils/helper";
+import { FACET_NAMES } from "../../../../utils/config";
+import { deployAndInit } from "../../../utils/deployAndInit";
+import { useFacet } from "../../../../utils/useFacet";
+import { getRandomUint256 } from "../../../utils/helper";
 import {
   AccountFacet,
   ERC20Mock,
@@ -14,7 +14,7 @@ import {
   TsERC20,
   TsFaucet,
   ZkTrueUp,
-} from "../../../typechain-types";
+} from "../../../../typechain-types";
 
 const fixture = async () => {
   const res = await deployAndInit(FACET_NAMES);
