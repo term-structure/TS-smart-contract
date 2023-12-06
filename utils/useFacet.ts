6fdf0fa9f74b56392a99948c0fd530dfe8335e3e
Ray Huang <ray.huang@tkspring.com>
2023-12-06 04:59:51 +0000

    Revert "Merge branch 'dev' into 'main'"
    This reverts merge request !15

--- utils/useFacet.ts
+++
@@ -1,6 +0,0 @@
-import { ethers } from "hardhat";
-
-export const useFacet = async (facetName: string, diamondAddr: string) => {
-  const facet = await ethers.getContractAt(facetName, diamondAddr);
-  return facet;
-};
