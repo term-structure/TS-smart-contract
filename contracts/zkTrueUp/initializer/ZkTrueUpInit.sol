ef0492683c47effacc40f78eddc17644e1168aef
Ray Huang <ray880301@gmail.com>
2024-01-29 13:46:13 +0800

    rename INIT_BORROW_FEE_RATE

--- contracts/zkTrueUp/initializer/ZkTrueUpInit.sol
+++ contracts/zkTrueUp/initializer/ZkTrueUpInit.sol
@@ -104,7 +104,7 @@ contract ZkTrueUpInit is Ownable, Initializable, AccessControlInternal {
         // init loan facet
         LoanStorage.Layout storage lsl = LoanStorage.layout();
         lsl.halfLiquidationThreshold = InitialConfig.INIT_HALF_LIQUIDATION_THRESHOLD;
-        lsl.borrowFeeRate = InitialConfig.INIT_BORROWER_FEE_RATE;
+        lsl.borrowFeeRate = InitialConfig.INIT_BORROW_FEE_RATE;
         lsl.rollOverFee = InitialConfig.INIT_ROLL_OVER_FEE;
 
         LiquidationFactor memory initLiquidationFactor = LiquidationFactor({
