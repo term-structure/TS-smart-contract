bbb96f41236495cc890402906d9ac57fcbcbf403
Ray Huang <ray880301@gmail.com>
2024-02-19 12:38:25 +0800

    fix

--- test/zkTrueUp/tsb/redeem.test.ts
+++ test/zkTrueUp/tsb/redeem.test.ts
@@ -369,7 +369,7 @@ describe("Redeem TsbToken", () => {
       const { v, r, s } = ethers.utils.splitSignature(signature);
 
       // increase time after maturity
-      await helpers.time.increaseTo(1672416510);
+      await helpers.time.increaseTo(1672416550);
       // redeem tsb token
       const redeemTx = await diamondTsbMock
         .connect(user2)
