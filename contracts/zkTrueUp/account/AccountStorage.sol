2dae8b685f98d31426e56a960bdace5fc0c920cb
Ray Huang <ray880301@gmail.com>
2024-04-26 17:14:36 +0800

    CVF-50

--- contracts/zkTrueUp/account/AccountStorage.sol
+++ contracts/zkTrueUp/account/AccountStorage.sol
@@ -15,7 +15,7 @@ library AccountStorage {
         mapping(address => uint32) accountIds;
         /// @notice Mapping of L2 Account Id => L1 Address
         mapping(uint32 => address) accountAddresses;
-        /// @notice LoanOwner => delegatee => isDelegated
+        /// @notice LoanOwner => delegatee => delegatedActions
         /// @dev User can delegate the right to operate the account to another address
         /// @dev Each delegated action has a unique bit in the delegatedActions mask
         mapping(address => mapping(address => uint256)) delegatedActions;
